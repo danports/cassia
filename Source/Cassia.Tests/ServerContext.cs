3d0419bb961e802b54da2b5dd5d4f68988ae0a34
danports <danports@86301f89-de55-0410-90a3-7b57f3730619>
2010-05-16 14:43:03 +0000

    Copy the test server files to the remote machine when running the tests.

--- Source/Cassia.Tests/ServerContext.cs
+++ Source/Cassia.Tests/ServerContext.cs
@@ -1,6 +1,8 @@
 using System;
+using System.IO;
 using System.ServiceModel;
 using System.ServiceProcess;
+using System.Threading;
 using Cassia.Tests.Model;
 
 namespace Cassia.Tests
@@ -23,14 +25,9 @@ namespace Cassia.Tests
             {
                 if (_testService == null)
                 {
-                    _serviceController = new ServiceController("CassiaTestServer", _server);
-                    _serviceController.Start();
-
-                    NetTcpBinding binding = new NetTcpBinding();
-                    binding.Security.Mode = SecurityMode.None;
-                    string remoteAddress = String.Format("net.tcp://{0}:17876/CassiaTestService", _server);
-                    _channelFactory = new ChannelFactory<IRemoteDesktopTestService>(binding, remoteAddress);
-                    _testService = _channelFactory.CreateChannel();
+                    CopyFilesToServer();
+                    StartService();
+                    ConnectToService();
                 }
                 return _testService;
             }
@@ -41,22 +38,81 @@ namespace Cassia.Tests
             get { return _server; }
         }
 
+        private string TargetDirectory
+        {
+            get { return string.Format(@"\\{0}\ADMIN$\Temp\CassiaTestServer", _server); }
+        }
+
         #region IDisposable Members
 
         public void Dispose()
         {
-            if (_channelFactory != null)
-            {
-                _channelFactory.Close();
-            }
-            if (_serviceController != null)
-            {
-                _serviceController.Stop();
-                _serviceController.Dispose();
-            }
+            DisconnectFromService();
+            StopService();
+            DeleteFilesFromServer();
         }
 
         #endregion
+
+        private void DeleteFilesFromServer()
+        {
+            if (!Directory.Exists(TargetDirectory))
+            {
+                return;
+            }
+            Directory.Delete(TargetDirectory, true);
+        }
+
+        private void StopService()
+        {
+            if (_serviceController == null)
+            {
+                return;
+            }
+            _serviceController.Stop();
+            _serviceController.WaitForStatus(ServiceControllerStatus.Stopped);
+            // It takes Windows a bit of time after the service stops to release locks on the assemblies, apparently.
+            Thread.Sleep(500);
+            _serviceController.Dispose();
+        }
+
+        private void DisconnectFromService()
+        {
+            if (_channelFactory == null)
+            {
+                return;
+            }
+            _channelFactory.Close();
+        }
+
+        private void CopyFilesToServer()
+        {
+            string targetDirectory = TargetDirectory;
+            if (!Directory.Exists(targetDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+            const string sourceDirectory = @"..\..\..\Cassia.Tests.Server\bin\Debug";
+            foreach (FileInfo fileInfo in new DirectoryInfo(sourceDirectory).GetFiles())
+            {
+                fileInfo.CopyTo(Path.Combine(targetDirectory, fileInfo.Name), true);
+            }
+        }
+
+        private void ConnectToService()
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Mode = SecurityMode.None;
+            string remoteAddress = String.Format("net.tcp://{0}:17876/CassiaTestService", _server);
+            _channelFactory = new ChannelFactory<IRemoteDesktopTestService>(binding, remoteAddress);
+            _testService = _channelFactory.CreateChannel();
+        }
+
+        private void StartService()
+        {
+            _serviceController = new ServiceController("CassiaTestServer", _server);
+            _serviceController.Start();
+        }
 
         public RdpConnection OpenRdpConnection()
         {
