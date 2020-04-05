e0973ea5a20661c0340809147843406e394a2fac
Dan Ports <dan@codewithin.com>
2020-04-05 11:57:04 -0700

    #84: Use the native AssemblyInfo generation bits. Migrate nuspec properties to MSBuild properties and update values (fixes #88).

--- Samples/SessionInfo/Source/SessionInfo/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Cassia SessionInfo Sample")]
--- Source/Cassia/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Cassia")]
-[assembly: CLSCompliant(true)]
